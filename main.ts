0b6690ab55fd6d64768b14c92d509e9f13f02e34
mworkfun <39076163+mworkfun@users.noreply.github.com>
2020-06-19 11:01:30 +0800

    update

--- main.ts
+++ main.ts
@@ -55,7 +55,7 @@ const enum IrButton {
  * Write the date: 2020-6-19
  */
 //% color="#ff6800" weight=10 icon="\uf1eb"
-namespace irReceiver {
+namespace irRemote {
     /**
      * define a IR receiver class
      */
